5d8ac38e3ed4804ee4109b44bfd3048aa2cd731b
Massimo Ludena <peruviangopher@gmail.com>
2021-11-20 16:24:40 -0500

    Adding check brackets challenge in go

--- 2_check_brackets/checkbrackets.go
+++ 2_check_brackets/checkbrackets.go
@@ -1,6 +1,48 @@
 package main
 
+import "fmt"
+
+type stack struct {
+	data []rune
+}
+
+func (s *stack) pop() rune {
+	output := s.data[len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
+	return output
+}
+
+func (s *stack) push(str rune) {
+	s.data = append(s.data, str)
+}
+
+var closePair = map[rune]rune{
+	'}': '{',
+	')': '(',
+	']': '[',
+}
+
+var inputs = []string{
+	"[()]{}{[()()]()}",
+	"[(])",
+}
 
 func main() {
+	for _, i := range inputs {
+		fmt.Println(solution(i))
+	}
+}
 
+func solution(s string) bool {
+	stk := stack{data: []rune{}}
+	for _, c := range []rune(s) {
+		if _, ok := closePair[c]; !ok {
+			stk.push(c)
+		} else {
+			if open := stk.pop(); open != closePair[c] {
+				return false
+			}
+		}
+	}
+	return len(stk.data) == 0
 }
