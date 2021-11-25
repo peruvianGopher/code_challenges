af9216f2cd6f37e692bb33b3a94a6c5d9a587550
Massimo Ludena <peruviangopher@gmail.com>
2021-11-24 20:45:21 -0500

    Move zeros go solution

---
+++ 8_move_zeros/CHALLENGE_8.md
@@ -0,0 +1,9 @@
+# CHALLENGE 8
+
+Given an array list of numbers 
+
+`[1,4,0,7,0,null,8,9,0,0,4]`
+
+Return a list with all the zeros moved to the end of the list, the numbers order should not be affected
+
+`[1,4,7,null,8,9,4,0,0,0,0]`
---
+++ 8_move_zeros/solution.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main()  {
+    input := []int{1,4,0,7,0,0,8,9,0,0,4}
+    fmt.Println(solution(input))
+}
+
+func solution(list []int) []int {
+    swapCounter := 0
+    for i, v := range list {
+        if v == 0 {
+            if swapCounter == 0 {
+                swapCounter = i + 1
+            }
+
+            for {
+                if swapCounter == len(list) {
+                    break
+                }
+
+                if list[swapCounter] != 0 {
+                    temp := list[i]
+                    list[i] = list[swapCounter]
+                    list[swapCounter] = temp
+                    break
+                }
+                swapCounter = swapCounter + 1
+            }
+        }
+    }
+    return list
+}
